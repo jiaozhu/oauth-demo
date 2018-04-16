c120e07e00e7768fce62b34cba78998f51c92e2d
Weijie Zhao <tttx@me.com>
2018-04-17 01:23:59 +0800

    更改入口类名称

--- src/main/java/io/predictech/oauthdemo/OauthDemoApp.java
+++ src/main/java/io/predictech/oauthdemo/OauthDemoApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OauthDemoApplication {
+public class OauthDemoApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(OauthDemoApplication.class, args);
+        SpringApplication.run(OauthDemoApp.class, args);
     }
 }
--- src/test/java/io/predictech/oauthdemo/OauthDemoAppTests.java
+++ src/test/java/io/predictech/oauthdemo/OauthDemoAppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OauthDemoApplicationTests {
+public class OauthDemoAppTests {
 
     @Test
     public void contextLoads() {
